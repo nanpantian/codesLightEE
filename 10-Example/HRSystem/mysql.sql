5d6390c5602f7d9c33b3c9726bda1715c4d26666
jack <jackjacklu@hotmail.com>
2013-07-23 06:09:36 +0800

    init commit

---
+++ 10-Example/HRSystem/mysql.sql
@@ -0,0 +1,112 @@
+drop database if exists hrSystem;
+
+create database hrSystem;
+
+use hrSystem;
+
+create table type_table
+(
+ type_id int auto_increment,
+ type_name varchar(50) not null,
+ amerce_amount double not null,
+ primary key(type_id)
+);
+describe  type_table;
+
+create table emp_table
+(
+ emp_id int auto_increment,
+ emp_type int,
+ emp_name varchar(50) not null,
+ emp_pass varchar(50) not null,
+ emp_salary double not null,
+ mgr_id int,
+ dept_name varchar(50),
+ primary key(emp_id),
+ unique key(emp_name),
+ foreign key(mgr_id) references emp_table(emp_id)
+);
+describe  emp_table;
+
+create table attend_table
+(
+ attend_id int auto_increment,
+ duty_day varchar(50) not null,
+ punch_time datetime,
+ is_come boolean not null,
+ type_id int not null,
+ emp_id int not null,
+ primary key(attend_id),
+ foreign key(type_id) references type_table(type_id),
+ foreign key(emp_id) references emp_table(emp_id)
+);
+describe  attend_table;
+
+create table app_table
+(
+ app_id int auto_increment,
+ attend_id int not null,
+ app_reason varchar(255),
+ app_result boolean,
+ type_id int not null,
+ primary key(app_id),
+ foreign key(type_id) references type_table(type_id),
+ foreign key(attend_id) references attend_table(attend_id)
+);
+describe  app_table;
+
+create table  pay_table
+(
+ pay_id int auto_increment,
+ pay_month varchar(50) not null,
+ pay_amount double not null,
+ emp_id int not null,
+ primary key(pay_id),
+ foreign key(emp_id) references emp_table(emp_id)
+);
+describe  pay_table;
+
+create table  check_table
+(
+ check_id int auto_increment,
+ app_id int not null,
+ check_result boolean not null,
+ check_reason varchar(255),
+ mgr_id int not null,
+ primary key(check_id),
+ foreign key(app_id) references app_table(app_id),
+ foreign key(mgr_id) references emp_table(emp_id)
+);
+describe  check_table;
+
+insert into type_table ( type_name , amerce_amount)
+	values ( 'normal', 0);
+insert into type_table ( type_name , amerce_amount)
+	values ( 'leave', -20);
+insert into type_table ( type_name , amerce_amount)
+	values ( 'ill', -10);
+insert into type_table ( type_name , amerce_amount)
+	values ( 'late', -10);
+insert into type_table ( type_name , amerce_amount)
+	values ( 'early', -10);
+insert into type_table ( type_name , amerce_amount)
+	values ( 'absence', -30);
+insert into type_table ( type_name , amerce_amount)
+	values ( 'on business', 10);
+select * from type_table;
+
+# 插入经理
+insert into emp_table (emp_type , emp_name , emp_pass , emp_salary , mgr_id , dept_name)
+	values (2, 'oracle', 'oracle' , 5000 , null , 'DB dept');
+insert into emp_table (emp_type , emp_name , emp_pass , emp_salary , mgr_id , dept_name)
+	values (2, 'weblogic', 'weblogic' , 6000 , null , 'Server dept');
+# 员工
+insert into emp_table (emp_type , emp_name , emp_pass , emp_salary , mgr_id)
+	values (1 , 'mysql', 'mysql' , 3000 , 1);
+insert into emp_table (emp_type , emp_name , emp_pass , emp_salary , mgr_id)
+	values (1 , 'hsql', 'hsql' , 3200 , 1);
+insert into emp_table (emp_type , emp_name , emp_pass , emp_salary , mgr_id)
+	values (1 , 'tomcat', 'tomcat' , 2800 , 2);
+insert into emp_table (emp_type , emp_name , emp_pass , emp_salary , mgr_id)
+	values (1 , 'jetty', 'jetty' , 2560 , 2);
+select * from emp_table;
